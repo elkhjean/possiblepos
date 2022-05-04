7a891dc911c07daa3a8536d44bfb244fab082730
Jean El Khoury <sean.elkhoury@gmail.com>
2022-05-05 00:10:31 +0200

    Final commit before hand in

--- src/main/java/se/kth/model/Reciept.java
+++ src/main/java/se/kth/model/Reciept.java
@@ -1,7 +1,10 @@
 package se.kth.model;
+
 import se.kth.DTOs.SaleDTO;
+
 /**
- * This is the reciept class it represents a reciept for one payment of one sale.
+ * This is the reciept class it represents a reciept for one payment of one
+ * sale.
  */
 public class Reciept {
     private SaleDTO finishedSale;
@@ -9,14 +12,20 @@ public class Reciept {
 
     /**
      * Constructor for the Reciept class
-     * @param finishedSale A DTO containing sale information
+     * 
+     * @param finishedSale     A DTO containing sale information
      * @param performedPayment A DTO containing payment information
      */
-    public Reciept(SaleDTO finishedSale, Payment performedPayment){
+    public Reciept(SaleDTO finishedSale, Payment performedPayment) {
         this.finishedSale = finishedSale;
         this.performedPayment = performedPayment;
     }
 
+    /**
+     * Getters
+     * 
+     * @return
+     */
     public SaleDTO getSaleDTO() {
         return this.finishedSale;
     }
